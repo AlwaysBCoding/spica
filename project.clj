2de8d6bf90a6b47db156b88c1c06c6d547df0d36
Jordan Leigh <AlwaysBCoding@gmail.com>
2015-09-07 23:36:37 -0700

    fixes url in project clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject alwaysbcoding/spica "0.0.2-SNAPSHOT"
   :description "A framework for building web applications with Clojure and Datomic"
-  :url "http://swish.i://github.com/AlwaysBCoding/spica"
+  :url "http://github.com/AlwaysBCoding/spica"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
